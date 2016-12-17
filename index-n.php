d1d168d68370be108f79bec822243422a213ecc4
giliv <dienasdarbs@inbox.lv>
2016-12-17 16:25:44 +0200

    Apdeits.

--- index-n.php
+++ index-n.php
@@ -7,15 +7,15 @@
 		<meta name="author" content="">
 		<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<link rel="stylesheet" href="assets/dist/css/lightbox.min.css">
-		<link rel="shortcut icon" href="assets/img/myicon.png" type="image/x-icon">
+		<link rel="stylesheet" href="./assets/dist/css/lightbox.min.css">
+		<link rel="shortcut icon" href="./assets/img/myicon.png" type="image/x-icon">
 		<title>Līvi</title>
 
 		<!-- Custom styles for this template -->
-		<link href="assets/css/style.css" rel="stylesheet">
-		<script src="assets/js/jquery.min.js"></script>
+		<link href="./css/style.css" rel="stylesheet">
+		<script src="./assets/js/jquery.min.js"></script>
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="./css/bootstrap.min.css" >
+		<link rel="stylesheet" href="./css/bootstrap.min.css">
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="./js/bootstrap.min.js"></script>
 	</head>
@@ -35,7 +35,7 @@
 			<div class="menu-wrap">
 				<ul>
 					<div class="logo"><img src="assets/img/logo.png" ></a></div>
-					<li class="border"><a href="#home" class="smoothScroll">JAUNUMI</a></li> <!-- Jaunumi -->
+					<li class="border"><a href="#home" class="smoothScroll" style="text-transform: uppercase;">Jaunumi</a></li> <!-- Jaunumi -->
 					<li class="border"><a href="#about" class="smoothScroll" style="text-transform: uppercase;">Diskogrāfija</a></li> <!-- Diskogrāfija --> 
 					<li class="border"><a href="#portfolio" class="smoothScroll" style="text-transform: uppercase;">Biogrāfija</a></li> <!-- Biogrāfija --> 
 					<li class="border"><a href="#contact" class="smoothScroll" style="text-transform: uppercase;">Galerijas</a></li> <!-- Galerijas-->
@@ -60,11 +60,11 @@
 		</nav>
 
 		<section id="home" name="home">
-			<div class="parallax" style="background-image: url('./assets/img/header-bg1.jpg')">
+			<div class="parallax" style="background-image: url('./assets/img/header-bg1.jpg'); height: 100vh;">
 				<div class="container" >
 					<div class="row">
 						<div class="col-xs-12 col-md-4">
-							<div class="guitar"><img id="cms1" class="cms-editable" src="assets\img\guitar1.png" style="width: 125px;height: 60%"></div>
+							<div class="guitar"><img id="cms1" class="cms-editable" src="assets/img/guitar1.png" style="width: 125px; height: 60%"></div>
 						</div>
 						<div class="col-xs-12 col-md-4">
 							<div class="home-title">
@@ -86,208 +86,16 @@
 			<div class="parallax" style="background-image: url('./assets/img/header-bg2.jpg')">
 				<div class="container" >
 					<div class="row">
-						<div class="col-xs-12 col-md-4">
-							df
+						<div class="col-xs-12 col-md-offset-4 col-md-6">
+							<div class="discography-title">
+								<h1>Diskogrāfija</h1>
+							</div>
+						<div class="col-xs-12 col-md-offset-4 col-md-6">
 						</div>
-						<div class="col-xs-12 col-md-4">
-							<div class="discography-title2">
-								<h1>DISKOGRĀFIJA</h1>
+							<img src="./assets/img/smalks/Smalks1.png" style="max-height: 170px; display: block; margin-left: auto; margin-right: auto">
+							<div class="discography-text">
+								<p>Grupa Līvi ir leģendāra latviešu rokgrupa, kas savu darbību uzsāka 1976. gadā Liepājā. Tā ir viena no visu laiku latviešu populārākajām grupām, kuru pārstāvējuši vairāki Latvijā labi zināmi mūziķi, bet pazīstamākie grupas dalībnieki bijuši Ainars Virga, Aivars Brīze, Ēriks Ķiģelis, Igo un Jānis Grodums. Grupa pastāvēšanas laikā izdevusi vairākus studijas albumus un dziesmu izlases, bet tās pazīstamākie skaņdarbi ir "Dzimtā valoda", "Dzelzsgriezējs", "Dieva dēls", "Piedod man" un citi.</p>
 							</div>
-							<div class="diskog">					
-						<div class="col-md-6 col-lg-6 col-xs-6">				<!-- šim būs jāsamaina īpašības, lai nefloato uz vienu pusi, bet gan ņem vērā kā vnk objektu, vajadzētu strādāt ar tām bootstrap 12 col, RESPEKTĪVI ŠIS BūS COL-LG-6 OR SMTH -->
-							
-
-
-
-							<div class='album'>		
-<div id="cms22" class="cms-editable">							
-								<div class='album-title'>					
-									IEDOMU PILSĒTA
-								</div>
-								</div>
-								<div class='contentorsmth'>
-									<div class='track-list'>
-										1. Mazais Sapnītis<br>
-										2. Saulgriežu Rīts<br>
-										3. Uz Lietiem<br>
-										4. Iedomu pilsēta<br>
-										5. Līviskā Ziņģe<br>
-										6. Rožu Laukums
-									</div>
-								<div id="cms8" class="cms-editable">
-									<img class='album-cover' src="assets\img\1.jpg" height= "169" width= "169"> 	
-								</div>
-								</div>
-							</div>
-
-
-
-							<div class='album'>		
-<div id="cms23" class="cms-editable">							
-								<div class='album-title'>					
-									K.M.K.V.P.
-								</div>
-								</div>
-								<div class='contentorsmth'>
-									<div class='track-list'>
-										1. Kādā Brīnišķā Dienā<br>
-										2. Es Karājos Tavā Bizē<br>
-										3. Zelta Sirds<br>
-										4. Meitene<br>
-										5. Var Jau Būt<br>
-										6. Kas Mums Ko Var Padarīt
-									</div>
-									<div id="cms9" class="cms-editable">
-									<img class='album-cover cms-editable' src="assets\img\3.jpg" height= "169" width= "167"> 	
-								</div>
-								</div>
-							</div>
-						
-						<div class='album'>		
-<div id="cms25" class="cms-editable">						<!--saliek visu viena divā un būs objekts, kurš ir respons -->
-								<div class='album-title'>					<!-- platums tāds pats kā contentorsmth -->
-								VIVA
-								</div>
-								</div>
-								<div class='contentorsmth'>
-								<div id="cms16" class="cms-editable">
-									<div class='track-list'>
-										1. Viva<br>
-										2. Tikai Tev un Man<br>
-										3. Ar Tevi Kopā<br>
-										4. Pēdējais Vilciens<br>
-										5. Ejam Tālāk<br>
-										6. NLO
-									</div>
-									</div>
-									<div id="cms10" class="cms-editable">
-									<img class='album-cover cms-editable' src="assets\img\5.jpg" height= "169" width= "169"> 
-									</div>
-							<!-- pieliec, lai floato uz labo pusi. vari arī viņu likt iekšā track-list divā, tava izvele -->
-								</div>
-							</div>
-
-						<div class='album'>		
-<div id="cms24" class="cms-editable">						<!--saliek visu viena divā un būs objekts, kurš ir respons -->
-								<div class='album-title'>					<!-- platums tāds pats kā contentorsmth -->
-									PĀRI VISAM
-								</div>
-								</div>
-								<div class='contentorsmth'>
-								<div id="cms17" class="cms-editable">
-									<div class='track-list'>
-										1. Nezināmais <br>
-										2. Pāri Visam <br>
-										3. Atdod Sevi Man<br>
-										4. Kāpēc? <br>
-										5. Ej, Vecais, Ej!<br>
-										6. Tu Piederi Man
-									</div>
-									</div>
-								<div id="cms11" class="cms-editable">
-									<img class='album-cover cms-editable' src="assets\img\7.jpg" height= "169" width= "169"> 
-								</div><!-- pieliec, lai floato uz labo pusi. vari arī viņu likt iekšā track-list divā, tava izvele -->
-								</div>
-							</div>
-					</div>
-					<div class='col-md-6 col-lg-6 col-xs-6'>
-						<div class='album'>	
-<div id="cms26" class="cms-editable">						<!--saliek visu viena divā un būs objekts, kurš ir respons -->
-								<div class='album-title'>					<!-- platums tāds pats kā contentorsmth -->
-								LĪVI
-								</div>
-								</div>
-								<div class='contentorsmth'>
-								<div id="cms18" class="cms-editable">
-									<div class='track-list'>
-										1. Pavasara Iestāšanās<br>
-										2. Deviņvīru Spēks<br>
-										3. Kurzeme<br>
-										4. Dzejnieks<br>
-										5. Saules Ceļš<br>
-										6. Gājputnu Sagaidīšana
-									</div>
-									</div>
-							<div id="cms12" class="cms-editable">
-									<img class='album-cover cms-editable' src="assets\img\2.jpg" height= "169" width= "169"> 
-						<!-- pieliec, lai floato uz labo pusi. vari arī viņu likt iekšā track-list divā, tava izvele -->
-								</div></div>
-							</div>
-
-							<div class='album'>		
-<div id="cms27" class="cms-editable">							<!--saliek visu viena divā un būs objekts, kurš ir respons -->
-								<div class='album-title'>					<!-- platums tāds pats kā contentorsmth -->
-								BAILES PAR ZIŅĢĒM
-								</div>
-								</div>
-								<div class='contentorsmth'>
-								<div id="cms19" class="cms-editable">
-									<div class='track-list'>
-										1. Bailes Par Ziņģi<br>
-										2. Zīlīte<br>
-										3. Saldus Saule<br>
-										4. Ziņģe Par Bailēm<br>
-										5. Vēju Muzikants<br>
-										6. Laika Dziesmiņa
-									</div>
-									</div>
-							<div id="cms13" class="cms-editable">
-									<img class='album-cover cms-editable' src="assets\img\4.jpg" height= "169" width= "169"> 	
-						</div><!-- pieliec, lai floato uz labo pusi. vari arī viņu likt iekšā track-list divā, tava izvele -->
-								</div>
-							</div>
-							<div class='album'>		
-<div id="cms28" class="cms-editable">							<!--saliek visu viena divā un būs objekts, kurš ir respons -->
-								<div class='album-title'>					<!-- platums tāds pats kā contentorsmth -->
-								SPOKU KOKS
-								</div>
-								</div>
-								<div class='contentorsmth'>
-								<div id="cms20" class="cms-editable">
-									<div class='track-list'>
-										1. Seskudāle <br>
-										2. Bruģakmeņi <br> 
-										3. Ostmalas Kvartālā <br>
-										4. Vai Tā Var Būt? <br> 
-										5. Ubaga Dziesma <br>
-										6. Vecais, Mums ir Laimējies 
-									</div>
-									</div>
-									<div id="cms14" class="cms-editable">
-									<img class='album-cover cms-editable' src="assets\img\6.jpg" height= "169" width= "160"> 
-								<!-- pieliec, lai floato uz labo pusi. vari arī viņu likt iekšā track-list divā, tava izvele -->
-								</div></div>
-							</div>
-
-							<div class='album'>		
-<div id="cms29" class="cms-editable">							<!--saliek visu viena divā un būs objekts, kurš ir respons -->
-								<div class='album-title'>					<!-- platums tāds pats kā contentorsmth -->
-								APRĪĻA PILIENI
-								</div>
-								</div>
-								<div class='contentorsmth'>
-								<div id="cms21" class="cms-editable">
-									<div class='track-list'>
-										1.Durvis<br>
-										2.Jānītis Pīpē<br>
-										3.Slimības<br>
-										4.Aprīļa Pilieni<br>
-
-										<br>
-										<br>
-									</div>
-									</div>
-									<div id="cms15" class="cms-editable">
-									<img class='album-cover cms-editable' src="assets\img\8.jpg" height= "169" width= "169"> 	
-							</div><!-- pieliec, lai floato uz labo pusi. vari arī viņu likt iekšā track-list divā, tava izvele -->
-								</div>
-							</div>
-						</div>		
-				
-			</div>
-						</div>
-						<div class="col-xs-12 col-md-4">
-							<img id="cms5" class="cms-editable" src="assets\img\guitar3.png" style="width:138px;433px;">
 						</div>
 					</div>
 				</div>
